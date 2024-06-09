aa4b40349ef58e66f82c01accf51e02da1b0ee21
maximt <vbmaxim@gmail.com>
2024-06-09 21:41:59 +0700

    app title

--- config/params.php
+++ config/params.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'title' => 'Управление персоналом',
+
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
