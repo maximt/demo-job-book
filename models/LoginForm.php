0157fe2d605c2e8d1dda98d99aefe7c56641c583
maximt <vbmaxim@gmail.com>
2024-06-09 22:12:08 +0700

    translate login page

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -33,6 +33,17 @@ class LoginForm extends Model
     }
 
     /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'username' => 'Имя пользователя',
+            'password' => 'Пароль',
+            'rememberMe' => 'Запомнить меня',
+        ];
+    }
+    /**
      * Validates the password.
      * This method serves as the inline validation for password.
      *
