2b5c46b0d6d69631c64632981959c7bea12d855a
maximt <vbmaxim@gmail.com>
2024-06-09 21:54:17 +0700

    auth only

--- controllers/EmployeeController.php
+++ controllers/EmployeeController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use app\models\Employee;
 use app\models\EmployeeJob;
 use app\models\search\EmployeeSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -22,6 +23,15 @@ class EmployeeController extends Controller
         return array_merge(
             parent::behaviors(),
             [
+                'access' => [
+                    'class' => AccessControl::class,
+                    'rules' => [
+                        [
+                            'allow' => true,
+                            'roles' => ['@'],
+                        ],
+                    ],
+                ],
                 'verbs' => [
                     'class' => VerbFilter::class,
                     'actions' => [
