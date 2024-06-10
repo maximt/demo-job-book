7e2004c799df0ea2c63fe6d698fbc6835f5cb90f
maximt <vbmaxim@gmail.com>
2024-06-10 18:54:09 +0700

    translation

--- views/site/error.php
+++ views/site/error.php
@@ -18,10 +18,10 @@ $this->title = $name;
     </div>
 
     <p>
-        The above error occurred while the Web server was processing your request.
+        Во время обработки запроса возникла ошибка.
     </p>
     <p>
-        Please contact us if you think this is a server error. Thank you.
+        Пожалуйста, попробуйте перегрузить страницу или свяжитесь с нами.
     </p>
 
 </div>
