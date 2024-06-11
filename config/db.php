c1410c4c565030a4bf5c99209ba9b925f4363155
maximt <vbmaxim@gmail.com>
2024-06-11 17:06:01 +0700

    db settings

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=test',
+    // 'dsn' => 'mysql:host=localhost;dbname=test', // for local
+    'dsn' => 'mysql:host=db;dbname=test', // for docker
     'username' => 'test',
     'password' => 'test',
     'charset' => 'utf8',
