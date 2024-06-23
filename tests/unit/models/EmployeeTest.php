61119eaee0c8e783ef2b38547aaa5214598ffac5
maximt <vbmaxim@gmail.com>
2024-06-23 16:58:28 +0700

    Employee tests

---
+++ tests/unit/models/EmployeeTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace tests\unit\models;
+
+use app\models\Employee;
+use Codeception\Test\Unit;
+
+class EmployeeTest extends Unit
+{
+
+    private function _createEmployee()
+    {
+        $employee = new Employee();
+        $employee->id = 1;
+        $employee->firstname = 'John';
+        $employee->lastname = 'Smith';
+        $employee->save();
+
+        return $employee;
+    }
+
+    public function testValidation()
+    {
+        $employee = new Employee();
+
+        $this->assertFalse($employee->validate(['firstname'])); // test empty fields
+        $this->assertFalse($employee->validate(['lastname']));
+
+        $employee->firstname = 'John';
+        $employee->surname = null; // not required
+        $employee->lastname = 'Doe';
+
+        $this->assertTrue($employee->validate());
+
+        $employee->gender = 2;
+        $this->assertFalse($employee->validate(['gender']));
+
+        $employee->gender = 1;
+        $this->assertTrue($employee->validate(['gender']));
+
+        $employee->birthday = 'not-a-date';
+        $this->assertFalse($employee->validate(['birthday']));
+
+        $employee->birthday = '1980-01-01';
+        $this->assertTrue($employee->validate(['birthday']));
+    }
+
+}
